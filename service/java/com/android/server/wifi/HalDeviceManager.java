93920dda5a0a286ba5eab6623790023378455197
Quang Anh Luong <qal@google.com>
2024-11-04 20:40:34 +0900

    Fix NPE caused by null value in mCachedStaticChipInfos
    HalDeviceManager#mCachedStaticChipInfos should not contain any null
    values, but this will happen if we get a JSONException in the middle of
    populating its values. Fix this by returning an empty array when we get
    a JSONException. The correct values will be populated again when the
    driver concurrency combo is loaded after STA bring up.
    
    Flag: EXEMPT bugfix
    
    Bug: 372474058
    Test: atest HalDeviceManagerTest
    Change-Id: I321a0033489dd14a7557334913bf4eca3dc300a5

--- service/java/com/android/server/wifi/HalDeviceManager.java
+++ service/java/com/android/server/wifi/HalDeviceManager.java
@@ -1378,6 +1378,7 @@ public class HalDeviceManager {
             }
         } catch (JSONException e) {
             Log.e(TAG, "Failed to load static chip info from store: " + e);
+            return new StaticChipInfo[0];
         }
         return staticChipInfos;
     }
