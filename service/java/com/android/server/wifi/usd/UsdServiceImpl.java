d52590ff4e29b8cac3ef662fb9b3e9a6f65ab193
maheshkkv <maheshkkv@google.com>
2024-11-06 11:57:50 -0800

    Add USD service
    Flag: android.net.wifi.flags.usd
    Bug: 340878198
    Test: atest FrameworksWifiTests
    Change-Id: Idaa26707bfe7d4d62dc60bfc05c61d8f33ee71c3

---
+++ service/java/com/android/server/wifi/usd/UsdServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wifi.usd;
+
+import android.content.Context;
+import android.net.wifi.usd.IUsdManager;
+import android.util.Log;
+
+import com.android.server.wifi.RunnerHandler;
+
+/**
+ * Implementation of the IUsdManager.
+ */
+public class UsdServiceImpl extends IUsdManager.Stub {
+    private static final String TAG = UsdServiceImpl.class.getName();
+    private final Context mContext;
+    private RunnerHandler mHandler;
+
+    /**
+     * Constructor
+     */
+    public UsdServiceImpl(Context context) {
+        mContext = context;
+    }
+
+    /**
+     * Start the service
+     */
+    public void start() {
+        Log.i(TAG, "start");
+    }
+
+    /**
+     * Start/initialize portions of the service which require the boot stage to be complete.
+     */
+    public void startLate() {
+        Log.i(TAG, "startLate");
+    }
+}
