03e9286f064934cd4df4e38c3cae1f035df29384
Peng Wang <wanpeng@google.com>
2024-11-19 14:23:28 -0800

    Remove current data capture logic
    Current logic:
        1. Create WifiUsabilityStatsEntry for each signal poll, add it into
           ring buffer
        2. Extract WifiUsabilityStatsEntries from ring buffer, label them and
           wrap it to WifiUsabilityStats, add WifiUsabilityStats into
           mWifiUsabilityStatsListGood or mWifiUsabilityStatsListBad
        3. Add mWifiUsabilityStatsListGood or mWifiUsabilityStatsListBad
           into mWifiLogProto. mWifiLogProto will be uploaded into Clearcut
    
    In this CL, we will remove logic#2, logic#3 above, that means:
        remove logic#2: Remove all function calls of addToWifiUsabilityStatsList
        remove logic#3: Remove mWifiUsabilityStatsListGood and
                        mWifiUsabilityStatsListBad logic in
    		    consolidateProto() and dump()
    
    In the mean time, we also need to remove some tests. For better tracking
    purpose, we records all the tests we modified:
    
    ClientModeImplTest: Remove verifications on addToWifiUsabilityStatsList
    call
    WifiMetricsTest
        testUpdateWifiUsabilityStatsEntries():
        verifyFirmwareAlertUpdatesWifiUsabilityMetrics():
            Since we remove logic#3, verifications will be re-implemented in
    	new data capture CLs.
        testWifiUsabilityStatsBufferSizeIsCapped(): Removed. Because
            mWifiUsabilityStatsListGood/Bad don't exists, will add similar
    	tests after new data capture is implemented.
        testWifiUsabilityStatsLabelGoodHasMinimumPeriod():
        testWifiUsabilityStatsLabelBadNotGeneratedGapLessThanMinimum():
        testWifiUsabilityStatsLabelBadGeneratedGapLargerThanMinimum():
        testGoodStatsAreGeneratedByWifiScoreBreachLow():
        testGoodStatsAreGeneratedByWifiUsabilityScoreBreachLow():
            Removed. Since addToWifiUsabilityStatsList logic no longer exists.
        verifyWifiDataStallUpdatesWifiUsabilityMetrics():
        verifyIpReachabilityLostUpdatesWifiUsabilityMetrics():
            Removed. Since LABEL_BAD logic will not exist.
    
    Flag: EXEMPT, new data capture logic will be implemented
    Bug: 377723852
    Test: atest com.android.server.wifi
    Change-Id: I1487c91b861efa6c36e5ea32f7a61af3e4fab32a

--- service/java/com/android/server/wifi/WifiMetrics.java
+++ service/java/com/android/server/wifi/WifiMetrics.java
@@ -4423,8 +4423,6 @@ public class WifiMetrics {
     public void logFirmwareAlert(String ifaceName, int errorCode) {
         incrementAlertReasonCount(errorCode);
         logWifiIsUnusableEvent(ifaceName, WifiIsUnusableEvent.TYPE_FIRMWARE_ALERT, errorCode);
-        addToWifiUsabilityStatsList(ifaceName, WifiUsabilityStats.LABEL_BAD,
-                WifiUsabilityStats.TYPE_FIRMWARE_ALERT, errorCode);
     }
 
     public static final String PROTO_DUMP_ARG = "wifiMetricsProto";
@@ -4998,23 +4996,6 @@ public class WifiMetrics {
                 pw.println("mWifiUsabilityStatsEntriesRingBuffer:");
                 for (WifiUsabilityStatsEntry stats : mWifiUsabilityStatsEntriesRingBuffer) {
                     printWifiUsabilityStatsEntry(pw, stats);
-                }
-                pw.println("mWifiUsabilityStatsList:");
-                for (WifiUsabilityStats stats : mWifiUsabilityStatsListGood) {
-                    pw.println("\nlabel=" + stats.label);
-                    pw.println("\ntrigger_type=" + stats.triggerType);
-                    pw.println("\ntime_stamp_ms=" + stats.timeStampMs);
-                    for (WifiUsabilityStatsEntry entry : stats.stats) {
-                        printWifiUsabilityStatsEntry(pw, entry);
-                    }
-                }
-                for (WifiUsabilityStats stats : mWifiUsabilityStatsListBad) {
-                    pw.println("\nlabel=" + stats.label);
-                    pw.println("\ntrigger_type=" + stats.triggerType);
-                    pw.println("\ntime_stamp_ms=" + stats.timeStampMs);
-                    for (WifiUsabilityStatsEntry entry : stats.stats) {
-                        printWifiUsabilityStatsEntry(pw, entry);
-                    }
                 }
 
                 pw.println("mMobilityStatePnoStatsMap:");
@@ -5781,23 +5762,6 @@ public class WifiMetrics {
             }
             mWifiLogProto.hardwareRevision = SystemProperties.get("ro.boot.revision", "");
 
-            // Postprocessing on WifiUsabilityStats to upload an equal number of LABEL_GOOD and
-            // LABEL_BAD WifiUsabilityStats
-            final int numUsabilityStats = Math.min(
-                    Math.min(mWifiUsabilityStatsListBad.size(),
-                            mWifiUsabilityStatsListGood.size()),
-                    MAX_WIFI_USABILITY_STATS_RECORDS_PER_TYPE_TO_UPLOAD);
-            LinkedList<WifiUsabilityStats> usabilityStatsGoodCopy =
-                    new LinkedList<>(mWifiUsabilityStatsListGood);
-            LinkedList<WifiUsabilityStats> usabilityStatsBadCopy =
-                    new LinkedList<>(mWifiUsabilityStatsListBad);
-            mWifiLogProto.wifiUsabilityStatsList = new WifiUsabilityStats[numUsabilityStats * 2];
-            for (int i = 0; i < numUsabilityStats; i++) {
-                mWifiLogProto.wifiUsabilityStatsList[2 * i] = usabilityStatsGoodCopy.remove(
-                        mRand.nextInt(usabilityStatsGoodCopy.size()));
-                mWifiLogProto.wifiUsabilityStatsList[2 * i + 1] = usabilityStatsBadCopy.remove(
-                        mRand.nextInt(usabilityStatsBadCopy.size()));
-            }
             mWifiLogProto.mobilityStatePnoStatsList =
                     new DeviceMobilityStatePnoScanStats[mMobilityStatePnoStatsMap.size()];
             for (int i = 0; i < mMobilityStatePnoStatsMap.size(); i++) {
@@ -7613,18 +7577,10 @@ public class WifiMetrics {
             }
             mWifiUsabilityStatsEntriesRingBuffer.add(wifiUsabilityStatsEntry);
             mWifiUsabilityStatsEntryCounter++;
-            if (mWifiUsabilityStatsEntryCounter >= NUM_WIFI_USABILITY_STATS_ENTRIES_PER_WIFI_GOOD) {
-                addToWifiUsabilityStatsList(ifaceName, WifiUsabilityStats.LABEL_GOOD,
-                        WifiUsabilityStats.TYPE_UNKNOWN, -1);
-            }
             if (mScoreBreachLowTimeMillis != -1) {
                 long elapsedTime =  mClock.getElapsedSinceBootMillis() - mScoreBreachLowTimeMillis;
                 if (elapsedTime >= MIN_SCORE_BREACH_TO_GOOD_STATS_WAIT_TIME_MS) {
                     mScoreBreachLowTimeMillis = -1;
-                    if (elapsedTime <= VALIDITY_PERIOD_OF_SCORE_BREACH_LOW_MS) {
-                        addToWifiUsabilityStatsList(ifaceName, WifiUsabilityStats.LABEL_GOOD,
-                                WifiUsabilityStats.TYPE_UNKNOWN, -1);
-                    }
                 }
             }
 
