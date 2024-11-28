68d6c7f560a8bf107c5985abc38f8d84f0dfac30
Gabriel Biren <gbiren@google.com>
2024-11-28 00:34:45 +0000

    Register death handler for the mainline supplicant service.
    Death handling logic is the same as in
    SupplicantStaIfaceHalAidlImpl.
    
    Bug: 365585450
    Flag: android.net.wifi.flags.usd
    Test: atest MainlineSupplicantTest
    Test: Manual tests
           1. Kill process from the command line
              and check for the death notification.
           2. Stop the process via the stopService
              method, and check that the count down
              latch is handled correctly.
    Change-Id: Ib21195f73a180b681481fa1498a868560c4e2490

--- service/tests/wifitests/src/com/android/server/wifi/mainline_supplicant/MainlineSupplicantTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/mainline_supplicant/MainlineSupplicantTest.java
@@ -19,12 +19,18 @@ package com.android.server.wifi.mainline_supplicant;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.net.wifi.util.Environment;
+import android.os.IBinder;
 import android.system.wifi.mainline_supplicant.IMainlineSupplicant;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -33,7 +39,11 @@ import org.mockito.MockitoAnnotations;
  */
 public class MainlineSupplicantTest {
     private @Mock IMainlineSupplicant mIMainlineSupplicantMock;
+    private @Mock IBinder mIBinderMock;
     private MainlineSupplicantSpy mDut;
+
+    private ArgumentCaptor<IBinder.DeathRecipient> mDeathRecipientCaptor =
+            ArgumentCaptor.forClass(IBinder.DeathRecipient.class);
 
     // Spy version of this class allows us to override methods for testing.
     private class MainlineSupplicantSpy extends MainlineSupplicant {
@@ -51,17 +61,46 @@ public class MainlineSupplicantTest {
     public void setUp() throws Exception {
         assumeTrue(Environment.isSdkAtLeastB());
         MockitoAnnotations.initMocks(this);
+        when(mIMainlineSupplicantMock.asBinder()).thenReturn(mIBinderMock);
         mDut = new MainlineSupplicantSpy();
+    }
+
+    private void validateServiceStart() throws Exception {
+        assertTrue(mDut.startService());
+        verify(mIBinderMock).linkToDeath(mDeathRecipientCaptor.capture(), anyInt());
+        assertTrue(mDut.isActive());
+    }
+
+    private void validateServiceStop() {
+        mDut.stopService();
+        mDeathRecipientCaptor.getValue().binderDied(mIBinderMock);
+        assertFalse(mDut.isActive());
     }
 
     /**
      * Verify that the class can be started and stopped successfully.
      */
     @Test
-    public void testStartAndStopSuccess() {
-        assertTrue(mDut.startService());
+    public void testStartAndStopSuccess() throws Exception {
+        validateServiceStart();
+        validateServiceStop();
+    }
+
+    /**
+     * Verify that unsolicited death notifications (ex. caused by a service crash)
+     * are handled correctly.
+     */
+    @Test
+    public void testUnsolicitedDeathNotification() throws Exception {
+        validateServiceStart();
+
+        // Notification with an unknown binder should be ignored
+        IBinder otherBinder = mock(IBinder.class);
+        mDeathRecipientCaptor.getValue().binderDied(otherBinder);
         assertTrue(mDut.isActive());
-        mDut.stopService();
+
+        // Notification with the correct binder should be handled
+        mDeathRecipientCaptor.getValue().binderDied(mIBinderMock);
         assertFalse(mDut.isActive());
     }
 }
